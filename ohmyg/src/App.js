9a7eeb3decad25fc519a3ae29afe19bd853a0958
REné-Marcel Zadi <rene.mzadi45@gmail.com>
2019-10-23 12:09:00 +0200

    Creation of Navbar

--- ohmyg/src/App.js
+++ ohmyg/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
+import Navbar from './components/layout/Navbar';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar  />
     </div>
   );
 }
