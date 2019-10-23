a08c4fe6e91deaec19fdc96f70d43b70ffa665fd
Elgur <leroy.maxence@yahoo.com>
2019-10-23 11:49:09 +0200

    created HomeNavButtons.css and .js and imported in App.js

--- ohmyg/src/App.js
+++ ohmyg/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
+import HomeNavButtons from './components/pages/homepage/HomeNavButtons';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <HomeNavButtons />
     </div>
   );
 }
