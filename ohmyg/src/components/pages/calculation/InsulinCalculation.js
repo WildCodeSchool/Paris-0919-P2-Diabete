075cab3ec31a1e61c140d031eee63ead4386e090
Pauline Drouhin <pauline.drouhin@gmail.com>
2019-10-23 18:39:28 +0200

    second style version with new div for each inputs and labels

--- ohmyg/src/components/pages/calculation/InsulinCalculation.js
+++ ohmyg/src/components/pages/calculation/InsulinCalculation.js
@@ -25,40 +25,40 @@ class InsulinCalculation extends Component {
   hyperglycemie = () => {
     return <div>
 
-     <p id="hyperglycemieWarning"> Attention, tu es en hyperglycémie. Rentre ces nouveaux paramètres :)</p>
-     <div className="hyperglycemie">
+      <p id="hyperglycemieWarning"> Attention, tu es en hyperglycémie. Rentre ces nouveaux paramètres :)</p>
+      <div className="hyperglycemie">
 
-      <div className="sensibilite">
-        <p className="inputTitle">Sensibilité : </p>
-        <div className="inputAndLabels">
-        <label>1 pour </label>
-        <input
-          id="sensibiliteinput"
-          type="number"
-          name="sensibilite"
-          onChange={this.handleChange}
-          value={this.state.sensibilite}
-        />
-        <label> g/l</label>
-      </div>
-      </div>
+        <div className="sensibilite">
+          <p className="inputTitle">Sensibilité : </p>
+          <div className="inputAndLabels">
+            <label>1 pour </label>
+            <input
+              id="sensibiliteinput"
+              type="number"
+              name="sensibilite"
+              onChange={this.handleChange}
+              value={this.state.sensibilite}
+            />
+            <label> g/l</label>
+          </div>
+        </div>
 
-      <div className="glycemieCible">
-        <p className="inputTitle">Glycémie cible : </p>
-        <div className="inputAndLabels">
-        <input
-          id="flecheinput"
-          type="number"
-          name="glycemiecible"
-          onChange={this.handleChange}
-          value={this.state.glycemiecible}
-        />
-        <label> g/l</label>
-      </div>
-      </div>
+        <div className="glycemieCible">
+          <p className="inputTitle">Glycémie cible : </p>
+          <div className="inputAndLabels">
+            <input
+              id="flecheinput"
+              type="number"
+              name="glycemiecible"
+              onChange={this.handleChange}
+              value={this.state.glycemiecible}
+            />
+            <label> g/l</label>
+          </div>
+        </div>
 
+      </div>
     </div>
-    </div> 
   }
 
   calculhypoglycemie = () => {
@@ -69,7 +69,7 @@ class InsulinCalculation extends Component {
 
     if (this.state.glycemie >= "0.80" && this.state.glycemie <= "1.20") {
       this.setState({ hypo: false });
-      this.setState({total : this.state.totalGlucides / this.state.ratio})
+      this.setState({ total: this.state.totalGlucides / this.state.ratio })
       return this.state.total
 
     }
@@ -78,7 +78,7 @@ class InsulinCalculation extends Component {
       this.setState({ hypo: false });
       const ecart = this.state.glycemie - this.state.glycemiecible
       const correction = ecart / this.state.sensibilite
-      this.setState({total : (this.state.totalGlucides / this.state.ratio)+correction} )
+      this.setState({ total: (this.state.totalGlucides / this.state.ratio) + correction })
       console.log(this.state.total)
       return this.state.total
 
@@ -91,48 +91,48 @@ class InsulinCalculation extends Component {
       <div className="insulinCalculationContainer">
 
         <div className="totalGlucides">
-        <div className="inputAndLabels">
           <p className="inputTitle">Mon Total de Glucides </p>
-          <input
-            id="totalGlucides"
-            type="number"
-            name="totalGlucides"
-            onChange={this.handleChange}
-            value={this.state.totalGlucides}
-          />
-          <label> g</label>
+          <div className="inputAndLabels">
+            <input
+              id="totalGlucides"
+              type="number"
+              name="totalGlucides"
+              onChange={this.handleChange}
+              value={this.state.totalGlucides}
+            />
+            <label> g</label>
           </div>
         </div>
 
         <div className="ratioAndGlycemie">
-        <div className="ratio">
-          <p className="inputTitle">Mon Ratio : </p>
-          <label htmlFor="totalGlucides">1 pour : </label>
-          <div className="inputAndLabels">
-          <input
-            id="ratio"
-            type="number"
-            name="ratio"
-            onChange={this.handleChange}
-            value={this.state.ratio}
-          />
-          <label> g de glucides</label>
+          <div className="ratio">
+            <p className="inputTitle">Mon Ratio : </p>
+            <div className="inputAndLabels">
+              <label htmlFor="totalGlucides">1 pour  </label>
+              <input
+                id="ratio"
+                type="number"
+                name="ratio"
+                onChange={this.handleChange}
+                value={this.state.ratio}
+              />
+              <label> g de glucides</label>
+            </div>
           </div>
-        </div>
 
-        <div className="glycemie">
-          <p className="inputTitle">Ma glycémie avant repas : </p>
-          <div className="inputAndLabels">
-          <input
-            id="glycemie"
-            type="number"
-            name="glycemie"
-            onChange={this.handleChange}
-            value={this.state.glycemie}
-          />
-          <label> g/l</label>
+          <div className="glycemie">
+            <p className="inputTitle">Ma glycémie avant repas : </p>
+            <div className="inputAndLabels">
+              <input
+                id="glycemie"
+                type="number"
+                name="glycemie"
+                onChange={this.handleChange}
+                value={this.state.glycemie}
+              />
+              <label> g/l</label>
+            </div>
           </div>
-        </div>
         </div>
 
         <div>
