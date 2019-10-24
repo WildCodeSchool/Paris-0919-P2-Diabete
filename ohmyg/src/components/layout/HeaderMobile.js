89a13bf644a0bc0c0c91bafc76d41838142fa113
Elgur <leroy.maxence@yahoo.com>
2019-10-24 17:31:20 +0200

    added logo.svg

--- ohmyg/src/components/layout/HeaderMobile.js
+++ ohmyg/src/components/layout/HeaderMobile.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './HeaderMobile.css'
+import MainLogo from '../../assets/pictures/logo_OMG.svg'
 
 const HeaderMobile = () => {
     return (
         <div className="headerBox">
+            <img src={MainLogo} alt="oh my g" className="header-title" />
         </div>
     )
 }
