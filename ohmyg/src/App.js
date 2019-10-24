2df8d408f4e5f49862830f7c35f43da4bd235d44
Elgur <leroy.maxence@yahoo.com>
2019-10-24 18:11:52 +0200

    Revert "prepared roote and switch"
    This reverts commit 6123eb06931d4ded5aa756120e7c5d7288726a9c.

--- ohmyg/src/App.js
+++ ohmyg/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-// import {Switch, Route} from "react-router-dom"
-
 import Footer from './components/layout/Footer';
 
 import './App.css';
@@ -10,10 +8,6 @@ const App = () => {
   return (
     <div className="App">
       <Footer />
-      {/* <Switch>
-        <Route exact path="/" components={Homepage}/>
-        <Route path="/characters" components={ContactUs}/>
-      </Switch> */}
     </div>
   )
 }
