d6e2ddbb535066230b2006435bfe956d168354e9
Elgur <leroy.maxence@yahoo.com>
2019-10-25 11:49:09 +0200

    app.js

--- ohmyg/src/App.js
+++ ohmyg/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
 
-import HeaderMobile from './components/layout/HeaderMobile';
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
 import ContentFrame from './components/pages/homepage/ContentFrame';
+import Footer from './components/layout/Footer';
 import HeaderMobile from './components/layout/HeaderMobile';
-
 import HomeNavButtons from './components/pages/homepage/HomeNavButtons';
+import Navbar from './components/layout/Navbar';
+
 
 
 import './App.css';
@@ -15,11 +14,6 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-
-      <HeaderMobile />
-
-      <HomeNavButtons />
-
       <Navbar  />
       <Footer />
       <HeaderMobile />
