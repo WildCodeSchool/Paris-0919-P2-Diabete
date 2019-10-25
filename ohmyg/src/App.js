fb2045cec41e2d01acc464c4ed46321faca17836
Elgur <leroy.maxence@yahoo.com>
2019-10-25 09:55:09 +0200

    app.js es5 to es6

--- ohmyg/src/App.js
+++ ohmyg/src/App.js
@@ -4,7 +4,7 @@ import HomeNavButtons from './components/pages/homepage/HomeNavButtons';
 
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <HomeNavButtons />
