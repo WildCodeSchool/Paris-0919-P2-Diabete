97a3aefb306b286c4f072a9c5de8777ab11b35f5
Fanny <fanny.bouffier@gmail.com>
2019-10-29 21:50:59 +0100

    fixed issues to get data from API

--- ohmyg/src/components/pages/meal/ApiRequest.js
+++ ohmyg/src/components/pages/meal/ApiRequest.js
@@ -9,8 +9,16 @@ class ApiRequest extends Component {
 
     getInfo = () => {
         axios.get('https://plateforme.api-agro.fr/api/records/1.0/search/?dataset=tables-ciqual&q=courgette&facet=origfdnm')
-        .then(res =>  this.setState ({foods:[res]}) ||   console.log(this.state.foods[0].data.records[0].fields.origfdnm) )
-    };
+      // Extract the DATA from the received response
+      .then(response => response.data)
+      // Use this data to update the state
+      .then(data => { console.log(data) ||
+        this.setState({
+            foods:data.records
+        });
+        console.log("state", this.state)
+    });
+}
 
 
     componentDidMount () {
@@ -18,24 +26,18 @@ class ApiRequest extends Component {
     } 
     
     render () {
-
+    
     return (
-        <div className="ApiRequest">
-          {/* {this.state.foods
-            // .filter(food => { return food[0].data.records[0].fields.origfdnm})
-            .map(food => ( */}
-              {/* <option>
-                {" "} */}
-               <p>Name:({this.state.foods[0].data.records[0].fields.origfdnm})</p> 
-              {/* </option> */}
-            {/* ))} */}
-      </div>
-    )
+        <select className="ApiRequest">
+            {this.state.foods.map(food => (
+                <option key={food.fields.origfdnm}>
+                {" "}
+                {food.fields.origfdnm}
+                </option>
+            ))}
+       </select>
+      )
     }
 }
-
-
-
-// { return food.food[0].data.records.fields.origfdnm === "courgette"})
 
 export default ApiRequest
