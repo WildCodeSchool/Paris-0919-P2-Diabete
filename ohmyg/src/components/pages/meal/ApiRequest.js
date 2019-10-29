43c4ef2e27074088f27322b486c0eeca16463903
Fanny <fanny.bouffier@gmail.com>
2019-10-29 23:04:17 +0100

    added variable into axios.get(url)

--- ohmyg/src/components/pages/meal/ApiRequest.js
+++ ohmyg/src/components/pages/meal/ApiRequest.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import axios from 'axios'
 
+    
+const ingr = "Courgette";
 
 class ApiRequest extends Component {
     state = {
@@ -8,7 +10,7 @@ class ApiRequest extends Component {
     };
 
     getInfo = () => {
-        axios.get('https://plateforme.api-agro.fr/api/records/1.0/search/?dataset=tables-ciqual&q=courgette&facet=origfdnm')
+        axios.get(`https://plateforme.api-agro.fr/api/records/1.0/search/?dataset=tables-ciqual&facet=origgpnm&q=${ingr}`)
       // Extract the DATA from the received response
       .then(response => response.data)
       // Use this data to update the state
@@ -29,14 +31,16 @@ class ApiRequest extends Component {
     
     return (
         <select className="ApiRequest">
-            {this.state.foods.map(food => (
+            {this.state.foods
+                // .filter(food => food.fields.origfdnm.includes("Barre"))
+                .map(food => (
                 <option key={food.fields.origfdnm}>
                 {" "}
                 {food.fields.origfdnm}
                 </option>
             ))}
        </select>
-      )
+        )
     }
 }
 
