b7a2fe44e80640efb67dcef6c175c4e53cc2863b
Charlotte LECONTE <charlotte.leconte91@gmail.com>
2019-10-29 17:29:51 +0100

    add the Glucides

--- ohmyg/src/components/pages/calculation/CarbCalculation.js
+++ ohmyg/src/components/pages/calculation/CarbCalculation.js
@@ -5,12 +5,16 @@ import './CarbCalculation.css'
 
 class CarbCalculation extends React.Component {
     state = {
-        food: 'poire',
+        name: 'poire',
         value:  100,
         carb100g: 20,
+        carbRatio: 20
     }
     
     handleChange = (event) => {
+        if (event.target.value > 500) {
+            return;
+          }
         this.setState({value: event.target.value});
     }
 
@@ -19,16 +23,26 @@ class CarbCalculation extends React.Component {
             <div className="range">
                 <InputRange
                     className="input-range"
-                    step={0.1}
+                    step={0.5}
                     maxValue={500}
                     minValue={0}
                     value={this.state.value}
                     onChange={value => this.setState({ value })} />
+
+                    <label>Poids </label>
                 <input 
                     className="foodWeight"
-                    type='number' 
+                    type='number'
+                    maxValue={500}
                     value={this.state.value}
                     onChange={this.handleChange} />
+                    <label> g</label>
+                
+                <input 
+                    className="carbohydrate"
+                    type='number'
+                    value={this.state.carb100g*this.state.value/100}
+                />
             </div>
         );
     }
