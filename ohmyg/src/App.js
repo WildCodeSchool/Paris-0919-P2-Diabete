88c1cd4093ce36e8d085d5a6cb52f0ce396400cf
simplonco <jean@gmail.com>
2019-11-04 11:13:31 +0100

    Home-page

--- ohmyg/src/App.js
+++ ohmyg/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
 import ContentFrame from './components/pages/homepage/ContentFrame';
-import HeaderMobile from './components/layout/HeaderMobile';
+/*import HeaderMobile from './components/layout/HeaderMobile';*/
 import HomeNavButtons from './components/pages/homepage/HomeNavButtons';
+import Footer from './components/layout/Footer';
 
 import './App.css';
 
@@ -12,10 +12,9 @@ const App = () => {
   return (
     <div className="App">
       <Navbar  />
-      <Footer />
-      <HeaderMobile />
       <HomeNavButtons />
       <ContentFrame />
+      <Footer />
     </div>
   );
 }
