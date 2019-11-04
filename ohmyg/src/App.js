1c012d931661b4aea657d57bf0a293ed8f387045
simplonco <jean@gmail.com>
2019-11-04 15:58:51 +0100

    food.js et css

--- ohmyg/src/App.js
+++ ohmyg/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import './App.css';
-import Meal from './components/pages/meal/meal';
+import Food from './components/pages/food/food';
 
 const App = () => {
   return (
     <div className="App">
-      <Meal  />
+      <Food  />
     </div>
   );
 }
---
+++ ohmyg/src/components/pages/food/food.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+import Navbar from '../../layout/Navbar';
+import ContentFrame from '../homepage/ContentFrame';
+import Footer from '../../layout/Footer';
+
+const Food = () => {
+    return (
+      <div className="App">
+        <Navbar  />
+        <ContentFrame />
+        <Footer />
+      </div>
+    );
+  }
+
+export default Food
