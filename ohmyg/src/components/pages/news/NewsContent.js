f78e7d78e7c125993a2f2a2bec6a383f586f1590
REné-Marcel Zadi <rene.mzadi45@gmail.com>
2019-11-13 17:23:07 +0100

    added picto and pictures

--- ohmyg/src/components/pages/news/NewsContent.js
+++ ohmyg/src/components/pages/news/NewsContent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./NewsContent.css";
 import Youtube from "react-youtube";
+import icon from "../../../assets/icons/pictog-coeur-39.png";
 
 class NewsContent extends React.Component {
 
@@ -48,7 +49,6 @@ class NewsContent extends React.Component {
 
 
         <div className="NewsContent-Article">
-          <img className="Open-book" src="https://image.flaticon.com/icons/svg/182/182321.svg" alt='Book' />
           <h2 className='firstArticleTitle'>Tout savoir sur le diabète</h2>
           <p className='textFirstArticle-block1'> Le diabète, c’est avoir trop de sucre dans le sang. On dit que c’est un diabète sucré. Attention ! On n’a pas un diabète sucré parce qu’on mange trop de sucre, comme on le croit trop souvent. 
           <br />
@@ -63,16 +63,17 @@ class NewsContent extends React.Component {
         </div>
 
         <div className="NewsContent-Article ">
-            <img className="Open-book" src="https://image.flaticon.com/icons/svg/182/182321.svg" alt='Book'/>
           <div className='NewsContent-block1'>
             <h2 className="Title-sport">Le sport et le diabète</h2>
-            <img className="Running-man" src="https://image.flaticon.com/icons/svg/808/808507.svg" alt='Running Man' />
           </div>
 
           <div className='newsContent-block2'>
             <h3 className="Title-sport">Bouger pour aller mieux</h3>
-            <p className="text-sportAndDiabete"> Le nageur américain Gary Hall plusieurs fois médaillé aux JO de Sydney en 2000 ou encore Chris Jarvis, rameur, médaillé d'or en championnat du monde... Tous les deux sont diabétiques et leur maladie ne les a pas empêchés d'exercer un sport à haut niveau. Et si leurs parcours sont impressionnants et forcent l'admiration, l'idée reste toutefois de trouver un sport adapté à votre condition physique.
-            Pour promouvoir la pratique du sport auprès des personnes diabétiques, la Fédération française de triathlon a mis en place un programme adapté. Des entraîneurs ont reçu une formation spécifique pour mieux accompagner les sportifs atteints de diabète.</p>
+            <div className='sportImageText'>
+              <img className="Running-man" src="http://www.blacktiemagazine.com/Society_2009_january/Juvenile_Diabetes_Gary_hall.jpg" alt='Running Man' />
+              <p className="text-sportAndDiabete1"> Le nageur américain Gary Hall plusieurs fois médaillé aux JO de Sydney en 2000 ou encore Chris Jarvis, rameur, médaillé d'or en championnat du monde... Tous les deux sont diabétiques et leur maladie ne les a pas empêchés d'exercer un sport à haut niveau. Et si leurs parcours sont impressionnants et forcent l'admiration, l'idée reste toutefois de trouver un sport adapté à votre condition physique.
+              Pour promouvoir la pratique du sport auprès des personnes diabétiques, la Fédération française de triathlon a mis en place un programme adapté. Des entraîneurs ont reçu une formation spécifique pour mieux accompagner les sportifs atteints de diabète.</p>
+            </div>
           </div>
           
           <div className='NewsContent-block3'>
@@ -96,6 +97,7 @@ class NewsContent extends React.Component {
       </div>
       
       <div className="NewsContent-Article">
+        <img className ="picto-coeur" src={icon} alt="pictoCoeur" />
         <h2 className="thirdArticleTitle">Coups de coeur de la semaine</h2>
         <img className="photo1" src="https://i.etsystatic.com/8886419/r/il/3940ae/1162144846/il_570xN.1162144846_k79i.jpg" alt='Pictures of bag'/>
         <p className="text-pocketInsuline">Aujourd'hui, nous vous présentons une jolie pochette "super mario" faite mains. Elle vous permets de ranger vos stylos à insuline et tout le nécessaire. Vous pouvez retrouver pleins d'autres modèles <a href='https://www.etsy.com/fr/search?q=pochettes+insuline' target='_blank'>ici</a>
