bced4d761147707e16b924ec1ddb79acf9089cae
Pauline Drouhin <pauline.drouhin@gmail.com>
2019-11-14 15:44:13 +0100

    improved page css (carrousel, list, sizes, responsive)

--- ohmyg/src/components/pages/calculation/CarbCalculation.js
+++ ohmyg/src/components/pages/calculation/CarbCalculation.js
@@ -19,7 +19,7 @@ class CarbCalculation extends React.Component {
         galleryItems: [],
         calculationButtonIsClicked: false,
         modifyingItem: false,
-        listButtonIsClicked : false
+        listButtonIsClicked: false
     }
 
 
@@ -82,63 +82,62 @@ class CarbCalculation extends React.Component {
             this.setState({ name: "" })
         }
 
-    
-        if (this.state.modifyingItem === true) { 
-            /* delete food*/ 
-            this.deleteItem()     
+
+        if (this.state.modifyingItem === true) {
+            /* delete food*/
+            this.deleteItem()
             /*added food with new values in tab*/
-            let carbRatioItem = (this.state.carb100g*this.state.value/100).toFixed(2)
-            this.setState({carbRatio: carbRatioItem}, async () => {
+            let carbRatioItem = (this.state.carb100g * this.state.value / 100).toFixed(2)
+            this.setState({ carbRatio: carbRatioItem }, async () => {
                 let objectName = this.state.name;
                 let objectValue = this.state.value;
                 let objectCarb100 = this.state.carb100g
                 let objectCarbRatio = this.state.carbRatio;
 
-                const tab = [...this.state.galleryItems,{dish: objectName, dishCarb: objectCarbRatio, dishWeight : objectValue, dishCarb100 : objectCarb100} ]
-                await this.setState({galleryItems: tab })
+                const tab = [...this.state.galleryItems, { dish: objectName, dishCarb: objectCarbRatio, dishWeight: objectValue, dishCarb100: objectCarb100 }]
+                await this.setState({ galleryItems: tab })
                 const result = (parseFloat(this.state.totalCarb) + parseFloat(this.state.carbRatio)).toFixed(2)
-                this.setState({totalCarb: result}) 
-            } )
-            this.setState({modifyingItem : false}) 
-            // document.getElementsByClassName("carbCalculation-active")[0].classList.remove("carbCalculation-active")
+                this.setState({ totalCarb: result })
+            })
+            this.setState({ modifyingItem: false })
 
-        } 
+        }
     }
-    
+
     /* Button delete */
-    
-    deleteItem =   () => {
-        this.setState({modifyingItem : false})
-        const tab=this.state.galleryItems
-        tab.splice(this.state.index,1)
-        this.setState({galleryItems: tab})
+
+    deleteItem = () => {
+        this.setState({ modifyingItem: false })
+        const tab = this.state.galleryItems
+        tab.splice(this.state.index, 1)
+        this.setState({ galleryItems: tab })
         const result = (parseFloat(this.state.totalCarb) - parseFloat(this.state.carbRatio)).toFixed(2)
-        this.setState({totalCarb: result}) 
+        this.setState({ totalCarb: result })
         document.getElementsByClassName("carbCalculation-active")[0].classList.remove("carbCalculation-active")
-    }  
-    
+    }
+
     calculationButton = () => {
         this.setState({ calculationButtonIsClicked: true })
     }
-    
+
     modifyItem = async (event, elem, index) => {
         console.log(event.target);
 
         event.target.className += " carbCalculation-active"
-        
-        await this.setState({modifyingItem : true})
-        this.setState({index: index})
-        this.setState({name : elem.dish})
-        this.setState({value: elem.dishWeight})
-        this.setState({carbRatio: elem.dishCarb})
-        this.setState({carb100g : elem.dishCarb100})        
+
+        await this.setState({ modifyingItem: true })
+        this.setState({ index: index })
+        this.setState({ name: elem.dish })
+        this.setState({ value: elem.dishWeight })
+        this.setState({ carbRatio: elem.dishCarb })
+        this.setState({ carb100g: elem.dishCarb100 })
         console.log(this.state.name, this.state.carb100g, this.state.carbRatio, this.state.value);
     }
-    
+
 
     render() {
         console.log(this.state.modifyingItem);
-        
+
         let carbRatio = (this.state.carb100g * this.state.value / 100).toFixed(2)
 
         const weight = this.state.value
@@ -219,17 +218,17 @@ class CarbCalculation extends React.Component {
                             <div className='carbs-list'>
                                 {this.state.galleryItems.map((elem, index) =>
                                     <ul className={this.state.modifyingItem ? "carbCalculation-ulModifying" : "carbCalculation-ul"} >
-                                        <div className= "carbCalculation-liItem "  onClick={(event) => this.modifyItem(event, elem, index)} key={index}>
-                                        {/* <div className={this.state.modifyingItem ? "carbCalculation-liItem" :"carbCalculation-liItemBis"} */}
+                                        <div className="carbCalculation-liItem " onClick={(event) => this.modifyItem(event, elem, index)} key={index}>
+                                            {/* <div className={this.state.modifyingItem ? "carbCalculation-liItem" :"carbCalculation-liItemBis"} */}
                                             <li> {elem.dish}</li>
                                             <li> {elem.dishCarb}</li>
                                         </div>
                                     </ul>)}
                             </div>
-                        </div>
-                        <div >
-                            <button onClick={this.calculationButton} className="meal-CalculationButton" id="#meal-CalculationButton">Calculation</button>
+                            <div >
+                                <button onClick={this.calculationButton} className="meal-CalculationButton" id="#meal-CalculationButton">Calculation</button>
 
+                            </div>
                         </div>
                     </div>
                 </div>
